428e0e74f8be27073600f0784efc108cb4d7d08f
tun35770 <tun35770@temple.edu>
2022-04-22 18:14:07 -0400

    JSON Server

--- src/App.js
+++ src/App.js
@@ -1,49 +1,90 @@
 import Header from './components/Header'
 import Stocks from './components/Stocks'
 import AddStock from './components/AddStock'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
   const [showAddStock, setShowAddStock] = useState(false)
-  const [stocks, setStocks] = useState([
-    {
-        id:1,
-        text: 'AAPL',
-        price: '$109.27',
-        active: true
-    },
-    {
-        id:2,
-        text: 'FB',
-        price: '$221.55',
-        active: true
-    },
-    {
-        id:3,
-        text: 'GME',
-        price: '$7.98',
-        active: true
+  const [stocks, setStocks] = useState([])
+
+  useEffect(() => {
+    const getStocks = async () => {
+      const stocksFromServer = await fetchStocks()
+      setStocks(stocksFromServer)
     }
-])
+
+    getStocks()
+  }, [])
+
+  //Fetch Stocks
+  const fetchStocks = async () => {
+    const res = await fetch('http://localhost:5000/stocks')
+    const data = await res.json()
+    
+    return data
+  }
+
+   //Fetch Stock
+   const fetchStock = async (id) => {
+    const res = await fetch(`http://localhost:5000/stocks/${id}`)
+    const data = await res.json()
+    
+    return data
+  }
 
   //Add Stock
-  const addStock = (stock) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newStock = { id, ...stock }
-    setStocks([...stocks, newStock])
+  const addStock = async (stock) => {
+    const res = await fetch('http://localhost:5000/stocks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(stock)
+    })
+
+    const data = await res.json()
+
+    setStocks([...stocks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newStock = { id, ...stock }
+    // setStocks([...stocks, newStock])
   }
 
   //Delete Stock
-  const deleteStock = (id) => {
+  const deleteStock = async (id) => {
+    await fetch(`http://localhost:5000/stocks/${id}`, {
+      method: 'DELETE'
+    })
+
     setStocks(stocks.filter((stock) => 
       stock.id !== id
     ))
   }
 
   //Toggle Active Status
-  const toggleActive = (id) => {
-    setStocks(stocks.map((stock => stock.id === id ? { ...stock, active: !stock.active} : stock)))
+  const toggleActive = async (id) => {
+    const stockToToggle = await fetchStock(id)
+    const updatedStock = { ...stockToToggle,
+                          active: !stockToToggle.active}
+
+                          const res = await fetch(`http://localhost:5000/stocks/${id}`,{
+                          method: 'PUT',
+                          headers: {
+                            'Content-Type': 'application/json'
+                          },
+                          body: JSON.stringify(updatedStock)
+                        })
+
+
+    const data = await res.json()
+
+    setStocks(
+      stocks.map((stock => 
+        stock.id === id ? { ...stock, active: data.active} : stock)
+      )
+    )
   }
 
   return (
