d991e1a2c804c3704699e3eb14266d1dc76b0c26
tun35770 <tun35770@temple.edu>
2022-04-28 16:28:21 -0400

    Stock border based on type

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,10 +1,22 @@
 import { FaTimes } from 'react-icons/fa'
 
+
+
 const Stock = ({ stock, onDelete, onDoubleClick }) => {
+  const getDivClasses = (stock) => {
+    let classNames = 'stock';
+    classNames += stock.changePercent > 0 ? ' increase' : 
+    stock.changePercent < 0 ? ' decrease' : ''
+
+    classNames += stock.autoUpdate ? ' auto-update' : ''
+  
+    classNames += stock.isCrypto ? ' type-crypto' : ' type-stock'
+
+    return classNames
+  }
+
   return (
-    <div className= {(stock.changePercent > 0 ? 'stock increase' : 
-                      stock.changePercent < 0 ? 'stock decrease' : 
-                      'stock') + (stock.autoUpdate ? ' auto-update' : '')}
+    <div className= {getDivClasses(stock)}
                       onDoubleClick = {() => onDoubleClick(stock.id)} >
         <h3>{stock.ticker} <FaTimes style={{color: 'red', 
                                         cursor: 'pointer' }} 
