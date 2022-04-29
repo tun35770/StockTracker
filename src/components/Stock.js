d8eacf8aa8aa229a244f825d7e41130311755ccc
tun35770 <tun35770@temple.edu>
2022-04-29 14:36:34 -0400

    Added green indicator for auto updates

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -8,23 +8,24 @@ const Stock = ({ stock, onDelete, onDoubleClick }) => {
     classNames += stock.changePercent > 0 ? ' increase' : 
     stock.changePercent < 0 ? ' decrease' : ''
 
-    classNames += stock.autoUpdate ? ' auto-update' : ''
-  
     classNames += stock.isCrypto ? ' type-crypto' : ' type-stock'
-
+  
     return classNames
   }
 
   return (
-    <div className= {getDivClasses(stock)}
-                      onDoubleClick = {() => onDoubleClick(stock.id)} >
-        <h3>{stock.ticker} <FaTimes style={{color: 'red', 
-                                        cursor: 'pointer' }} 
-            onClick={() => onDelete(stock.id)}/></h3>
-        <h2>{'$' + stock.price}</h2>
-        <p>{stock.date}</p>
-        <p>{stock.time ? ('Last Updated ' + stock.time) : ''}</p>
-        <p>{stock.formattedChange}</p>
+    <div className = 'stock-container'>
+      <div className = {stock.autoUpdate ? 'auto-update-indicator' : ''}></div>
+      <div className= {getDivClasses(stock)}
+                        onDoubleClick = {() => onDoubleClick(stock.id)} >
+          <h3>{stock.ticker} <FaTimes style={{color: 'red', 
+                                          cursor: 'pointer' }} 
+              onClick={() => onDelete(stock.id)}/></h3>
+          <h2>{'$' + stock.price}</h2>
+          <p>{stock.date}</p>
+          <p>{stock.time ? ('Last Updated ' + stock.time) : ''}</p>
+          <p>{stock.formattedChange}</p>
+      </div>
     </div>
   )
 }
